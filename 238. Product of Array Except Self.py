ae061645e787b3a39f97be90a74e43b345679df6
ALTHD12 <adithyalalit@gmail.com>
2026-07-01 23:26:34 +0530

    Prefix Product/Suffix Product technique

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -1,4 +1,4 @@
-#Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+''' Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
 
 The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 
@@ -13,7 +13,7 @@ Output: [24,12,8,6]
 Example 2:
 
 Input: nums = [-1,1,0,-3,3]
-Output: [0,0,9,0,0]
+Output: [0,0,9,0,0] '''
 
 
 class Solution:
